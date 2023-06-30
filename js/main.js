f21e06f25ed2ccaf5ec5732c44fa8775a9cf3a14
solcasa <solcasa8789@gmail.com>
2023-06-30 13:02:10 +0900

    230630

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(function () {
+
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        sct > 0 ? $('.header').addClass('on') : $('.header').removeClass('on');
+    });
+
+    $('.main_slide').slick({
+        arrows: false,
+        autoplay: true,
+        fade: true,
+        dots: true,
+    });
+
+
+    $('.family_link span').on('click', function () {
+        $(this).toggleClass('on');
+        $(this).next().toggleClass('on');
+    });
+
+    $('.scr').on('click', function (e) {
+        e.preventDefault();
+        const st = $(this.hash).offset().top;
+        console.log(st);
+        $('html, body').animate({ scrollTop: st }, 600)
+    });
+
+    $(function () {
+        $('.to_top').on('click', function () {
+            $('html, body').animate({ scrollTop: 0 }, 500);
+            return false;
+        });
+    });
+})

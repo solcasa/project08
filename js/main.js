3a58493cf949879e0de66eead9dc9c3f93bbdf66
Gyuri Kim <133613660+solcasa@users.noreply.github.com>
2023-08-29 14:19:31 +0900

    230829
    230829

--- js/main.js
+++ js/main.js
@@ -1,21 +1,9 @@
 $(function () {
 
-    $(window).on('scroll', function () {
-        let sct = $(window).scrollTop();
-        sct > 0 ? $('.header').addClass('on') : $('.header').removeClass('on');
-    });
-
     $('.main_slide').slick({
         arrows: false,
         autoplay: true,
         fade: true,
-        // dots: true,
-    });
-
-
-    $('.family_link span').on('click', function () {
-        $(this).toggleClass('on');
-        $(this).next().toggleClass('on');
     });
 
     $('.scr').on('click', function (e) {
@@ -25,10 +13,16 @@ $(function () {
         $('html, body').animate({ scrollTop: st }, 600)
     });
 
-    $(function () {
-        $('.to_top').on('click', function () {
-            $('html, body').animate({ scrollTop: 0 }, 500);
-            return false;
-        });
+    $('.family_link span').on('click', function () {
+        $(this).toggleClass('on');
+        $(this).next().toggleClass('on');
     });
-})
+
+    $('.to_top').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+
+});
+
+

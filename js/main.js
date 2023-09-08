e43094165a8179d0a5aeeb94ec897e2d3b2065e4
Gyuri Kim <133613660+solcasa@users.noreply.github.com>
2023-09-08 18:11:15 +0900

    230908 res
    230908 res

--- js/main.js
+++ js/main.js
@@ -23,6 +23,22 @@ $(function () {
         return false;
     });
 
+    $('.header .gnb>ul>li>a').on('click', function (e) {
+        if ($('.header .gnb').hasClass('on')) {
+            e.preventDefault();
+            $(this).next().stop().slideToggle();
+        }
+    });
+
+    $('.bar_menu').on('click', function () {
+        $('.header .gnb').toggleClass('on');
+    });
+
+    $(window).on('resize', function () {
+        $('.header .gnb').removeClass('on');
+        $('.sub').removeAttr('style');
+    })
+
 });
 
 
